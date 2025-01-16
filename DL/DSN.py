a7009e062b1827b115d8c614349031054f384f3d
Dongyang Li <133027184+wosida@users.noreply.github.com>
2025-01-16 22:29:55 +0800

    Add files via upload

--- DL/DSN.py
+++ DL/DSN.py
@@ -1,4 +1,6 @@
+#深度监督举例,引入辅助loss
 import torch.nn as nn
+import torch
 # 定义卷积块
 # 包含3x3卷积+BN+relu
 def conv3x3_bn_relu(in_planes, out_planes, stride=1):
@@ -6,38 +8,61 @@ def conv3x3_bn_relu(in_planes, out_planes, stride=1):
     return nn.Sequential(
             nn.Conv2d(in_planes, out_planes, kernel_size=3,
                       stride=stride, padding=1, bias=False),
+            nn.Conv2d(out_planes, out_planes, kernel_size=3,
+                      stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_planes),
             nn.ReLU(inplace=True),
             )
 
+#深度监督
+class DSNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1=conv3x3_bn_relu(3,64)
+        self.pool1=nn.MaxPool2d(2)
+        self.conv2=conv3x3_bn_relu(64,128)
+        self.pool2=nn.MaxPool2d(2)
+        self.aufc1=nn.Linear(128,10)
+        self.conv3=conv3x3_bn_relu(128,256)
+        self.pool3=nn.MaxPool2d(2)
+        self.conv4=conv3x3_bn_relu(256,512)
+        self.pool4=nn.AdaptiveAvgPool2d((1,1))
+        self.fc1=nn.Linear(512,1024)
+        self.fc2=nn.Linear(1024,10)
+    def forward(self,x):
+        x=self.conv1(x)
+        x=self.pool1(x)
+        x=self.conv2(x)
+        x=self.pool2(x)
+        aux1=nn.AdaptiveAvgPool2d((1,1))(x)
+        aux1=aux1.view(aux1.size(0),-1)
+        aux1=self.aufc1(aux1)
+        x=self.conv3(x)
+        x=self.pool3(x)
+        x=self.conv4(x)
+        x=self.pool4(x)
+        x=x.view(x.size(0),-1)
+        x=self.fc1(x)
+        y=self.fc2(x)
+        return y,aux1
 
-class C1DeepSup(nn.Module):
-    def __init__(self, num_class=150, fc_dim=2048, use_softmax=False):
-        super(C1DeepSup, self).__init__()
-        self.use_softmax = use_softmax
-        self.cbr = conv3x3_bn_relu(fc_dim, fc_dim // 4, 1)
-        self.cbr_deepsup = conv3x3_bn_relu(fc_dim // 2, fc_dim // 4, 1)
-        # 最后一层卷积
-        self.conv_last = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)
-        self.conv_last_deepsup = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)
+def train(epochs,trainloader):
+    net=DSNet()
+    criterion=nn.CrossEntropyLoss()
+    optimizer=torch.optim.Adam(net.parameters())
+    for epoch in range(epochs):
+        for i,data in enumerate(trainloader,0):
+            inputs,labels=data
+            optimizer.zero_grad()
+            outputs,aux1=net(inputs)
+            loss1=criterion(outputs,labels)
+            loss2=criterion(aux1,labels)
+            loss=loss1+0.4*loss2
+            loss.backward()
+            optimizer.step()
+            print(f'epoch:{epoch},step:{i},loss:{loss.item()}')
 
-    # 前向计算流程
-    def forward(self, conv_out, segSize=None):
-        conv5 = conv_out[-1]
-        x = self.cbr(conv5)
-        x = self.conv_last(x)
-        if self.use_softmax:  # is True during inference
-            x = nn.functional.interpolate(
-                x, size=segSize, mode='bilinear', align_corners=False)
-            x = nn.functional.softmax(x, dim=1)
-            return x
-        # 深监督模块
-        conv4 = conv_out[-2]
-        _ = self.cbr_deepsup(conv4)
-        _ = self.conv_last_deepsup(_)
 
-        # 主干卷积网络softmax输出
-        x = nn.functional.log_softmax(x, dim=1)
-        # 深监督分支网络softmax输出
-        _ = nn.functional.log_softmax(_, dim=1)
-        return (x, _)
+
+
+
